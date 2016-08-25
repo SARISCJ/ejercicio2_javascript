1ae2f8d13ac6fb92a32d7049f7358bb63e86c04c
SARISCJ <saracastillo0@gmail.com>
2016-08-25 15:33:58 -0500

    segundo ejercicio

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ejercicio 2
+
+###### Pseudocodigo  
+
+1. Inicio
+2. Leer datos
+	Dato1
+	Dato2
+	Dato3
+3. Promediar datos
+4. Entregar datos
+5. Fin 
+
+### Diagrama de flujo
+
+![Sin titulo](http://i65.tinypic.com/312yfli.jpg)
---
+++ js/app.js
@@ -0,0 +1,5 @@
+var nota1 = prompt("ingresa tu primera nota");
+var nota2 = prompt("ingresa tu segunda nota");
+var nota3 = prompt("ingresa tu tercera nota");
+var promedio = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3))/3;
+window.alert  ("El promedio de tus notas es: " + promedio);
